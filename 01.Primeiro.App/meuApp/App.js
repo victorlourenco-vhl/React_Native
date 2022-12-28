7011914932d74b9b8f2fddc149dec9429f76b749
Victor Lourenço <victor.lourenco@tgid.com.br>
2022-12-27 21:40:58 -0300

    LEARN: Utilizando o state

--- 01.Primeiro.App/meuApp/App.js
+++ 01.Primeiro.App/meuApp/App.js
@@ -1,33 +1,17 @@
-import React from 'react';
-import { Image, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, Text, View, Button } from 'react-native';
 
 function App() {
-  
+  const [valor, setValor] = useState(false);
   return (
     <View>
-      <Text>Olá, Mundo!</Text>
-      <Text>Meu primeiro App</Text>
-      <Text style={{ color: 'red', fontSize: 30 }}>Victor Hugo Lourenço</Text>
-      <Image
-        source={{ uri: 'https://via.placeholder.com/150' }}
-        style={{ width: 300, height: 300 }}
-      />
-      <Jobs largura={100} altura={200} nome='Jobs'/>
+      <Button title='Clicar' onPress={() => setValor(!valor)}/>
+      <Text>{valor?'Victor':'Lourenço'}</Text>
     </View>
   );
 }
 
-function Jobs({largura, altura, nome}) {
-  let img = 'https://sujeitoprogramador.com/steve.png';
-  return (
-    <View>
-      <Image 
-        source={{uri : img}}
-        style={{width: largura, height: altura}}
-      />
-      <Text>{nome}</Text>
-    </View>
-  )
-}
+
+
 
 export default App;
