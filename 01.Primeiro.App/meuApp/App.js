9dd940ba393084144958c67eb1756d07e9a7800f
Victor Lourenço <victor.lourenco@tgid.com.br>
2023-01-07 14:05:11 -0300

    FEAT: Trabalhando com alinhamentos e tamanhos dinâmicos

--- 01.Primeiro.App/meuApp/App.js
+++ 01.Primeiro.App/meuApp/App.js
@@ -1,17 +1,49 @@
 import React, { useState } from 'react';
-import { Image, Text, View, Button } from 'react-native';
+import { Image, Text, View, StyleSheet, Button, TextInput} from 'react-native';
 
 function App() {
   const [valor, setValor] = useState(false);
   return (
-    <View>
+    <View style={styles.area}>
       <Button title='Clicar' onPress={() => setValor(!valor)}/>
-      <Text>{valor?'Victor':'Lourenço'}</Text>
+      <Text >{valor?'Victor':'Lourenço'}</Text>
+      
+      <Text style={[styles.alinhaTexto, styles.tamanhoFonte]}>Eu sou o texto 1</Text>
+      <Text>Eu sou o texto 2</Text>
+      <Text>Eu sou o texto 3</Text>
+      <Text style={styles.alinhaTexto}>Eu sou o texto 4</Text>
+
+      
+
+      <View style={{ flex: 1, flexDirection: 'row', justifyContent:'space-around', alignItems: 'center'}}>
+        <View style={{width:50, height:50, backgroundColor:'green'}}></View>
+        <View style={{width:50, height:50, backgroundColor:'red'}}></View>
+        <View style={{width:50, height:50, backgroundColor:'blue'}}></View>
+        <View style={{width:50, height:50, backgroundColor:'yellow'}}></View>
+      </View>
+      
+
     </View>
   );
 }
 
 
 
+const styles = StyleSheet.create({
+  area:{
+    marginTop: 40,
+    flex: 1
+  },
+  alinhaTexto:{
+    textAlign: 'center'
+  },
+  tamanhoFonte:{
+    fontSize:25,
+    color: '#FF0000'
+  }
+});
+export default App
 
-export default App;
+
+
+
